de76c6cd52f12bad83a47a269b1f7dc2552969f7
AlBaash247 <138034243+AlBaash247@users.noreply.github.com>
2025-11-27 11:31:54 +0400

    # cam + player controls + optmize

--- player.gd
+++ player.gd
@@ -1,23 +1,56 @@
 extends CharacterBody3D
 
 
-const SPEED = 5.0
-const JUMP_VELOCITY = 4.5
+const SPEED : float = 9.0
+const JUMP_VELOCITY : int = 12
+
+var xform : Transform3D
 
 
 func _physics_process(delta: float) -> void:
+	
+	# rotate camera left
+	if Input.is_action_just_pressed("cam_left"):
+		$CameraController.rotate_y(deg_to_rad(-30))
+	
+	# rotate camera right
+	if Input.is_action_just_pressed("cam_right"):
+		$CameraController.rotate_y(deg_to_rad(30))
+		
+
 	# Add the gravity.
 	if not is_on_floor():
 		velocity += get_gravity() * delta
-
+	
+	handle_palyer_movment()
+	
+	# make CameraController Match the position of the player
+	$CameraController.position = lerp($CameraController.position, position, 0.16)
+	
+	
+func handle_palyer_movment() -> void:
+	
 	# Handle jump.
 	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
-
+	
 	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
-	var input_dir := Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")
-	var direction := (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
+	var input_dir = Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")
+	
+	# make player character move in relation of camera rotaiton
+	var direction = ($CameraController.transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
+	
+	# rotate carachter in relation to the camera
+	if input_dir != Vector2(0,0):
+		$MeshInstance3D.rotation_degrees.y = $CameraController.rotation_degrees.y - rad_to_deg(input_dir.angle()) - 90
+		
+	# rotate player character to match floor
+	if is_on_floor():
+		match_floor_rotation($RayCast3D.get_collision_normal())
+	else:
+		match_floor_rotation(Vector3.UP)
+	
+	# player character movement
 	if direction:
 		velocity.x = direction.x * SPEED
 		velocity.z = direction.z * SPEED
@@ -26,3 +59,14 @@ func _physics_process(delta: float) -> void:
 		velocity.z = move_toward(velocity.z, 0, SPEED)
 
 	move_and_slide()
+	
+	
+func match_floor_rotation(floor_normal : Vector3):
+	xform = global_transform
+	xform.basis.y = floor_normal
+	xform.basis.x = -xform.basis.z.cross(floor_normal)
+	xform.basis = xform.basis.orthonormalized()
+	
+	#match floor rotation smoothly
+	global_transform = global_transform.interpolate_with(xform, 0.3)
+	
