5527e82200ff7d60a1685c197ddc14692731c94c
Albaash247 <albaash247@gmail.com>
2025-11-28 16:03:28 +0400

    # fallzone

--- player.gd
+++ player.gd
@@ -7,6 +7,7 @@ const JUMP_VELOCITY : int = 12
 var xform : Transform3D
 
 
+
 func _physics_process(delta: float) -> void:
 	
 	# rotate camera left
@@ -30,6 +31,8 @@ func _physics_process(delta: float) -> void:
 	
 func handle_palyer_movment() -> void:
 	
+	
+	
 	# Handle jump.
 	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
@@ -37,8 +40,12 @@ func handle_palyer_movment() -> void:
 	# Get the input direction and handle the movement/deceleration.
 	var input_dir = Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")
 	
+
+	
 	# make player character move in relation of camera rotaiton
 	var direction = ($CameraController.transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
+	
+	$RayCast3D.position = global_position
 	
 	# rotate carachter in relation to the camera
 	if input_dir != Vector2(0,0):
@@ -70,3 +77,8 @@ func match_floor_rotation(floor_normal : Vector3):
 	#match floor rotation smoothly
 	global_transform = global_transform.interpolate_with(xform, 0.3)
 	
+
+
+func _on_fall_zone_body_entered(body: Node3D) -> void:
+	#reset game if player fall off
+	get_tree().change_scene_to_file("res://level_1.tscn")
